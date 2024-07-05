a448c9cdba495e027aeee233ce19ffe39875ec05
Rahul <rahul.mishraa2108@example.com>
2024-07-05 15:45:33 +0530

    Adding file

--- js/script.js
+++ js/script.js
@@ -3,11 +3,10 @@ $(document).ready(function(){
         loop:true,
         margin:10,
         responsiveClass:true,
-        center: true,
 
         responsive:{
             0:{
-                items:1,
+                items:3,
             },
             767:{
                 items:3,
@@ -23,12 +22,11 @@ $(document).ready(function(){
         loop:true,
         margin:10,
         responsiveClass:true,
-        center: true,
 
         responsive:{
             0:{
                 items:1,
-                // slideBy:1,
+                slideBy:1,
             },
             768:{
                 items:2,
