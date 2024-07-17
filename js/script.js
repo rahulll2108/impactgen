bc27c4360ae459bb2b829d6be4fafe2985872f62
Rahul <rahul.mishraa2108@example.com>
2024-07-17 16:11:06 +0530

    Adding new changes

--- js/script.js
+++ js/script.js
@@ -38,3 +38,20 @@ $(document).ready(function(){
         }
     })
 })
+
+const counterNum = document.querySelectorAll(".counter-numbers");
+
+const speed = 200;
+counterNum.forEach((curElem)=>{
+    const updateNumber = ()=>{
+        const targetNumber = parseInt(curElem.dataset.numbers);
+        const initialNum = parseInt(curElem.innerText);
+        const incrementNumber = Math.trunc(targetNumber/speed);
+        if (initialNum < targetNumber) {
+            curElem.innerText = initialNum+incrementNumber;
+
+            setTimeout(updateNumber, 10);
+        }
+    };
+    updateNumber();
+});
